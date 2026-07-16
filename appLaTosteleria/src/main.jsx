6c6caff8f6f404ccbb1f20aa9c9cb0beb9dc773d
M4TiUX <matiaschacon912@gmail.com>
2026-07-15 20:52:11 -0600

    Eliminé las clases y archivos relacionados con appMovie. En el apartado de ventanas y forms, controladores y modelos, así como su eliminación en el llamado en  el index.php. Lo que quedó son las clases de User e imagen

--- appLaTosteleria/src/main.jsx
+++ appLaTosteleria/src/main.jsx
@@ -6,17 +6,6 @@ import { createBrowserRouter } from "react-router-dom";
 import { Home } from "./components/Home/Home";
 import { RouterProvider } from "react-router";
 import { PageNotFound } from "./components/Home/PageNotFound";
-import { ListMovies } from "./components/Ventanas/ListMovies";
-import { DetailMovie } from "./components/Ventanas/DetailMovie";
-import ListRentals from "./components/Rental/ListRentals";
-import DetailRental from "./components/Rental/DetailRental";
-import TableMovies from "./components/Ventanas/TableMovies";
-import { CreateMovie } from "./components/Ventanas/CreateMovie";
-import { UpdateMovie } from "./components/Ventanas/UpdateMovie";
-import { CatalogMovies } from "./components/Ventanas/CatalogMovies";
-import { MovieUploadImage } from "./components/Ventanas/MovieUploadImage";
-import { CreateMovieRental } from "./components/Rental/CreateMovieRental";
-import { GraphRetal } from "./components/Rental/GraphRental";
 import UserProvider from "./components/User/UserProvider";
 import { Unauthorized } from "./components/User/Unauthorized";
 import { Login } from "./components/User/Login";
@@ -52,51 +41,8 @@ const rutas = createBrowserRouter([
         path: "/",
         element: <Auth requiredRoles={["Administrador"]} />,
         children: [
-          {
-            path: "/movie-table",
-            element: <TableMovies />,
-          },
-          {
-            path: "/movie/crear/",
-            element: <CreateMovie />,
-          },
-          {
-            path: "/movie/update/:id",
-            element: <UpdateMovie />,
-          },
+          
         ],
-      },
-      {
-        path: "/movie/",
-        element: <ListMovies />,
-      },
-      {
-        path: "/catalog-movies/",
-        element: <CatalogMovies />,
-      },
-      {
-        path: "/movie/:id",
-        element: <DetailMovie />,
-      },
-      {
-        path: "movie/image/",
-        element: <MovieUploadImage />,
-      },
-      {
-        path: "/rental",
-        element: <ListRentals />,
-      },
-      {
-        path: "/retal/:id",
-        element: <DetailRental />,
-      },
-      {
-        path: "/rental/crear/",
-        element: <CreateMovieRental />,
-      },
-      {
-        path: "/rental/graph",
-        element: <GraphRetal />,
       },
       {
         path: "/unauthorized",
